4c67e3d48da0edd38a38382038e215bf2153d153
401 <401@academicos.cesde.edu.co>
2022-05-02 14:55:27 -0500

    se modificaron clases

--- classes/Persona.py
+++ classes/Persona.py
@@ -1,10 +1,45 @@
 class Persona:
 
     def _init_(self):
-        self.nombre=None
-        self.edad=None
-        self.telefono=None
+        self.__nombre=None
+        self.__edad=None
+        self.__telefono=None
+
+    #getters
+    @property
+    def nombre(self):
+        return(self.__nombre)
+
+    @property
+    def edad(self):
+        return(self.__edad)
+
+    @property
+    def telefono(self):
+        return(self.__telefono)
+
+    #Setters
+    #decorador de clase 
+    @nombre.setter
+    def nombre(self,nombre):
+        self.__nombre=nombre
+       
+    @edad.setter
+    def edad(self,edad):
+        if(edad<0):
+          self.__edad=None 
+          print("edad erronea") 
+        else:
+            self.__edad=edad
+           
+
+    @telefono.setter
+    def telefono(self,telefono):
+        self.__telefono=telefono
+        
+       
 
     def saludar(self):
-        print(f'Hola me llamo {self.nombre}')
-        print(f'mi edad es {self.edad} y tengo sueño')
+        print(f'Hola me llamo {self.__nombre}')
+        print(f'mi edad es {self.__edad} y tengo sueño')
+
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 #se crean objetos de la clase
+
+from classes.Persona import Persona
+persona=Persona()
+
+persona.edad =400
+print(persona.edad)

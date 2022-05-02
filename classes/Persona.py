d957ceb2bc95a7da8eb9fc0c896bba53470de02a
401 <401@academicos.cesde.edu.co>
2022-05-02 13:22:15 -0500

    HU002-ADNY OSORIO

---
+++ classes/Persona.py
@@ -0,0 +1,9 @@
+class Persona:
+    def __init_(self):
+        self.nombre==None
+        self.edad==None
+        self.telefono==None
+    def saludar(self):
+        print(f'hola me llamo: {self.nombre}')
+        print(f'mi edad es:  {self.edad}')
+        print(f'mi telefono es: {self.telefono}')    
